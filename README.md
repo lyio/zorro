bde2d8d0fe5fb846b0d28b816349b1983fcbeb3f
Danny Hanke <dannyhanke1988@gmail.com>
2016-02-15 23:29:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Finally working on zorro!
+
+Using angular2.0 for the frontend
+
+No idea for the backend yet
---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'zorro',
+    template: '<h1>My First Zorro 2</h1>'
+})
+
+export class AppComponent { };
